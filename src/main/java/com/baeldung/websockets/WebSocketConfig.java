32e046bfc2fd68c36f7a2de46bd59d7325909a38
benice7029 <benice7029@gmail.com>
2022-12-28 02:22:29 +0800

    change to allow all cros

--- src/main/java/com/baeldung/websockets/WebSocketConfig.java
+++ src/main/java/com/baeldung/websockets/WebSocketConfig.java
@@ -18,12 +18,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(final StompEndpointRegistry registry) {
-    	registry.addEndpoint(new String[] { "/chat/1" });
-    	/* 22 */     registry.addEndpoint(new String[] { "/chat/1" }).setAllowedOrigins(new String[] { "http://localhost:4200", "http://localhost:80", "http://localhost:8081", "https://ge.helpermgmt.com", "https://ge-erp.myadam.xyz" }).withSockJS();
-    	/* 23 */     registry.addEndpoint(new String[] { "/chat/2" });
-    	/* 24 */     registry.addEndpoint(new String[] { "/chat/2" }).setAllowedOrigins(new String[] { "http://localhost:4200", "http://localhost:80", "http://localhost:8081", "https://ge.helpermgmt.com", "https://ge-erp.myadam.xyz" }).withSockJS();
-    	/* 25 */     registry.addEndpoint(new String[] { "/chatwithbots" });
-    	/* 26 */     registry.addEndpoint(new String[] { "/chatwithbots" }).setAllowedOrigins(new String[] { "http://localhost:4200", "http://localhost:80", "http://localhost:8081", "https://ge.helpermgmt.com", "https://ge-erp.myadam.xyz" }).withSockJS();
+    	registry.addEndpoint(new String[] { "/chat/1" }).setAllowedOriginPatterns("*").withSockJS();
+    	registry.addEndpoint(new String[] { "/chat/2" }).setAllowedOriginPatterns("*").withSockJS();
+    	registry.addEndpoint(new String[] { "/chatwithbots" }).setAllowedOriginPatterns("*").withSockJS();
     }
 
 }
